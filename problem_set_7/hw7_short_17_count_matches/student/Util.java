e8cb2792851df634f2e2df8920a4a42aa6730ada
mrquatsch@gmail.com <mrquatsch@gmail.com>
2014-03-03 00:57:03 -0600

    cleaned up dir names

---
+++ problem_set_7/hw7_short_17_count_matches/student/Util.java
@@ -0,0 +1,19 @@
+//
+// Complete this method to return the number of zeros in the int array parameter
+//
+
+public class Util
+{
+    public int getCount(int[] values)
+    {
+        int count = 0;
+        for (int current : values)
+        {
+            // TODO: count the number of zeros
+            if(current == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

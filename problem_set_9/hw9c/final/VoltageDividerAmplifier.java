567a98b30ef95a62b5026eed43a9af89dccd3b3d
mrquatsch <mrquatsch@gmail.com>
2014-03-14 06:42:14 -0500

    Finished problem_set9

--- problem_set_9/hw9c/final/VoltageDividerAmplifier.java
+++ problem_set_9/hw9c/final/VoltageDividerAmplifier.java
@@ -1,7 +1,15 @@
-public class VoltageDividerAmplifier
+public class VoltageDividerAmplifier extends Amplifier
 {
      // TODO: supply the implementation of VoltageDividerAmplifier as a subclass of Amplifier
      // R1 and R2 represent resistance. The gain (g) is calculated from the resistance as follows:
      // VoltageDividerAmplifier:    g = R2 / (R1 + R2)
      // TODO: the getGain method needs to return the correct gain (g)
+
+    public VoltageDividerAmplifier(int resistance1, int resistance2) {
+        super(resistance1, resistance2);
+    }
+
+    public double getGain() {
+        return (double)getR2() / (getR1() + (double)getR2());
+    }
 }
